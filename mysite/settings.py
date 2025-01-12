9dd7452b31a2928e15e9f938d7063b83e87ba3e0
Vinayak Gupta <130219730+VinayakGPT@users.noreply.github.com>
2025-01-12 18:52:54 +0530

    Ready to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -137,3 +138,5 @@ STATICFILES_DIRS = [BASE_DIR / "static",]
 
 # Directory for collected static files
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
